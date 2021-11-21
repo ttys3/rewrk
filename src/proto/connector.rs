37853034ab911f14521cf97e46f34dd6609962d7
荒野無燈 <ttys3.rust@gmail.com>
2021-11-22 04:49:30 +0800

    build: update tls related crates
    ```
    rustls = "0.19.1" -> rustls = "0.20.2"
    tokio-rustls = "0.22.0" -> tokio-rustls = "0.23.1"
    rustls-native-certs = "0.5.0" -> rustls-native-certs = "0.6.1"
    ```

--- src/proto/connector.rs
+++ src/proto/connector.rs
@@ -6,7 +6,7 @@ use crate::utils::BoxedFuture;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio::task::JoinHandle;
 
-use tokio_rustls::webpki::{DNSName, DNSNameRef};
+use rustls::ServerName;
 use tokio_rustls::TlsConnector;
 
 use hyper::client::conn;
@@ -43,13 +43,13 @@ impl Connect for HttpConnector {
 }
 
 pub struct HttpsConnector {
-    domain: DNSName,
+    domain: ServerName,
     tls_connector: TlsConnector,
 }
 
 impl HttpsConnector {
     pub fn new(domain: &str, alpn: &[Vec<u8>]) -> Result<Self, AnyError> {
-        let domain = DNSNameRef::try_from_ascii_str(domain)?.to_owned();
+        let domain = ServerName::try_from(domain)?;
         let tls_connector = tls::connector_from_alpn(alpn)?;
 
         Ok(Self {
@@ -68,7 +68,7 @@ impl Connect for HttpsConnector {
         Box::pin(async move {
             let stream = self
                 .tls_connector
-                .connect(self.domain.as_ref(), stream)
+                .connect(self.domain.clone(), stream)
                 .await?;
 
             handshake(stream, protocol).await
--- src/proto/tls.rs
+++ src/proto/tls.rs
@@ -2,18 +2,25 @@ use crate::error::AnyError;
 
 use std::sync::Arc;
 
-use rustls::ClientConfig;
+use rustls::{ClientConfig, RootCertStore};
 use rustls_native_certs::load_native_certs;
 use tokio_rustls::TlsConnector;
 
 pub fn connector_from_alpn(alpn: &[Vec<u8>]) -> Result<TlsConnector, AnyError> {
-    let mut config = ClientConfig::new();
+    let mut root_cert_store = RootCertStore::empty();
+    let root_ca = load_native_certs().map_err(|_| "cant load native certificates")?;
+    for cert in root_ca {
+        root_cert_store.add(&rustls::Certificate(cert.0))?;
+    }
 
-    config.root_store = load_native_certs().map_err(|_| "cant load native certificates")?;
+    let mut config = ClientConfig::builder()
+        .with_safe_defaults()
+        .with_root_certificates(root_cert_store)
+        .with_no_client_auth();
 
-    config.set_protocols(alpn);
+    config.alpn_protocols = alpn.into();
 
-    let connector = TlsConnector::from(Arc::new(config));
+    let connector = Arc::new(config).into();
 
     Ok(connector)
 }
